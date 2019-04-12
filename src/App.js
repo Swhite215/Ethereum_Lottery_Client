5ccda1ef7a05b701877248e6dfb0840f65bd5666
Spencer <swhite215@live.com>
2019-04-12 10:23:24 -0400

    added lottery call in lifecycle for owner address

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import web3 from "./web3";
+import lottery from "./lottery";
 import "./App.css";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            manager: ""
+        };
+    }
+    async componentDidMount() {
+        //Don't have to specify from property with MetaMask
+        const manager = await lottery.methods.manager().call();
+
+        this.setState({
+            manager
+        });
+    }
+
     render() {
         window.addEventListener("load", async () => {
             // Modern dapp browsers...
@@ -32,20 +48,8 @@ class App extends Component {
         });
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
+                <h2>Lottery Contract</h2>
+                <p>This contract is managed by {this.state.manager}</p>
             </div>
         );
     }
