601b1c964b10887a18bd185a3e54a5af78c0eb07
Spencer <swhite215@live.com>
2019-04-11 22:03:16 -0400

    updated web3 to address metamask breaking change

--- src/web3.js
+++ src/web3.js
@@ -1,6 +1,6 @@
 import Web3 from "web3";
 
 //Creates instance of web3 and passes provider from MetaMask library
-const web3 = new Web3(window.web3.currentProvider);
+const web3 = new Web3(window.ethereum);
 
 export default web3;
